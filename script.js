c936391d3c0ed02fd7195fc0d59d7ab6dfad8c52
Imed Adel <aadelimed@gmail.com>
2021-05-19 15:42:09 +0000

    Init

---
+++ script.js
@@ -0,0 +1,60 @@
+const fs = require("fs")
+const readline = require("readline")
+
+const path = "words.dict"
+
+try {
+	fs.unlinkSync(path)
+	//file removed
+} catch (err) {
+	console.error(err)
+}
+
+const stream = fs.createWriteStream("words.dict", { flags: "a" })
+
+var rd = readline.createInterface({
+	input: fs.createReadStream("./en-dump.json"),
+	// output: process.stdout,
+	// console: false,
+})
+
+const wrds = new Set()
+
+console.log(new Date().toISOString())
+rd.on("line", function (line) {
+	const parsed = JSON.parse(line)
+	if (
+		[
+			"affix",
+			"prefix",
+			"suffix",
+			"circumfix",
+			"infix",
+			"interfix",
+			"phrase",
+			"adv_phrase",
+			"punct",
+		].includes(parsed.pos)
+	) {
+		return
+	}
+
+	if (Array.isArray(parsed.forms)) {
+		for (const { form } of parsed.forms) {
+			wrds.add(form)
+		}
+	}
+})
+
+rd.on("close", () => {
+	console.log(new Date().toISOString())
+	stream.write(wrds.size + "\n")
+
+	console.log(new Date().toISOString())
+	for (const wrd of wrds) {
+		stream.write(wrd + "\n")
+	}
+
+	console.log(new Date().toISOString())
+	stream.end()
+})
